92d8888e8291547a28b622f10ff2a9e0d3aac723
juangm1990 <jgm0106@alu.medac.es>
2025-03-19 18:45:00 +0100

    commit preparación examen

---
+++ SegundoTrimestre/EjerciciosExtra/BolaExtra_Objetos/Delantero.java
@@ -0,0 +1,20 @@
+package SegundoTrimestre.EjerciciosExtra.BolaExtra_Objetos;
+
+public class Delantero extends Jugador implements Entrenable{
+    
+    public Delantero (String nombre, int numeroCamiseta, String posicion) {
+        super(nombre, numeroCamiseta, posicion);
+    }
+
+    //Métodos
+    public void entrenar() {
+        System.out.println("Dispara al arco");
+    }
+
+    public void accionEspecial() {
+        System.out.println("¡Gooooooool!¡Ha marcado el penalti!");
+    }
+
+
+
+}
