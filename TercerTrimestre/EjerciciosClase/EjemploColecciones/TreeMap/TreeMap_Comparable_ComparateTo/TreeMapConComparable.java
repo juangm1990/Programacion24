be53ec88218ed8c9e3cd6afcbb45886bfbc58cf8
juangm1990 <jgm0106@alu.medac.es>
2025-04-03 16:55:09 +0200

    TreeSet

---
+++ TercerTrimestre/EjerciciosClase/EjemploColecciones/TreeMap/TreeMap_Comparable_ComparateTo/TreeMapConComparable.java
@@ -0,0 +1,23 @@
+package TercerTrimestre.EjerciciosClase.EjemploColecciones.TreeMap.TreeMap_Comparable_ComparateTo;
+
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapConComparable {
+    public static void main(String[] args) {
+        // Clave: Persona (ordenada por nombre), Valor: edad
+        TreeMap<Persona, Integer> personas = new TreeMap<>();
+
+        personas.put(new Persona("Carlos"), 30);
+        personas.put(new Persona("Ana"), 25);
+        personas.put(new Persona("Beatriz"), 28);
+        personas.put(new Persona("Luis"), 35);
+        personas.put(new Persona("Ana"), 27); // Sobrescribe porque compara por nombre
+
+        System.out.println("Personas y edades (orden alfabético):");
+        for (Map.Entry<Persona, Integer> entrada : personas.entrySet()) {
+            System.out.println("Persona: " + entrada.getKey() + " - Edad: " + entrada.getValue());
+        }
+    }
+}
---
+++ TercerTrimestre/EjerciciosClase/EjemploColecciones/TreeSet/TreeSet_Comparable_ComparateTo/Fruta.java
@@ -0,0 +1,23 @@
+package TercerTrimestre.EjerciciosClase.EjemploColecciones.TreeSet.TreeSet_Comparable_ComparateTo;
+
+public class Fruta implements Comparable<Fruta> {
+    private String nombre;
+
+    public Fruta(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public int compareTo(Fruta otra) {
+        return this.nombre.compareTo(otra.nombre); // Orden alfabético
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
