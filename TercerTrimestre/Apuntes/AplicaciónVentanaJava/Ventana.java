ac47078d5dd9c8849c0f67ee0ba62ca1d6020c38
juangm1990 <jgm0106@alu.medac.es>
2025-04-08 19:32:20 +0200

    Ventanajava

---
+++ TercerTrimestre/Apuntes/AplicaciónVentanaJava/Ventana.java
@@ -0,0 +1,20 @@
+package TercerTrimestre.Apuntes.AplicaciónVentanaJava;
+
+import javax.swing.JFrame;
+
+public class Ventana {
+    
+       public static void main(String[] args) throws Exception {
+
+        JFrame frame = new JFrame("Mi primera ventana en Java");
+        frame.setSize(500, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+        
+        
+    }
+}
